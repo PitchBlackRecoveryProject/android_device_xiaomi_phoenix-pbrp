61cd11b8406da8ac3773d0647d1d7a0fbc066ad4
AndroiableDroid <androiabledroid@gmail.com>
2020-10-12 21:55:38 +0530

    phoenix: bug fixes
    Signed-off-by: Manjot Sidhu <manjot.techie@gmail.com>

--- omni_phoenix.mk
+++ omni_phoenix.mk
@@ -23,7 +23,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 # Inherit from our custom product configuration
 $(call inherit-product, vendor/pb/config/common.mk)
 
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,$(LOCAL_PATH)/recovery/root,recovery/root)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,$(LOCAL_PATH)/recovery/root,recovery/root) \
+    $(LOCAL_PATH)/prebuilt/dtb:dtb.img
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := phoenix
