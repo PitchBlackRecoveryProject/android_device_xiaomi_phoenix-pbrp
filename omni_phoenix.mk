49addd52a7833c099ef727b5b5e2718c56300c98
Himanshu Raj <himanshurajkush@gmail.com>
2023-12-24 22:07:37 +0530

    phoenix: Enable Project Quotas and Case Folding
    Change-Id: Iafe7383dfd2d0a93cdaf1b70af8fcf536559bb60

--- omni_phoenix.mk
+++ omni_phoenix.mk
@@ -22,6 +22,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 # Inherit from this product for devices that support only 64-bit apps using:
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
 
+# Enable Project Quotas and Case Folding
+$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
+
 # Inherit from phoenix device
 $(call inherit-product, device/xiaomi/phoenix/device.mk)
 
